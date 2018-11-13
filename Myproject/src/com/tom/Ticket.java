5cb8385e89056e189df2329c7302d9acd851a5d7
user <user@es302n34>
2018-11-13 11:58:53 +0800

    = =

---
+++ Myproject/src/com/tom/Ticket.java
@@ -0,0 +1,18 @@
+package com.tom;
+
+import java.util.Scanner;
+
+public class Ticket {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Please enter number of tickets:");
+		int tickets = scanner.nextInt();
+		System.out.print("How many round-trip tickets:");
+		int round = scanner.nextInt();
+		
+		
+
+	}
+
+}
